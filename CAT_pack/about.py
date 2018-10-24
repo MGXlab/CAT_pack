2a93185f26345398cd7c5014f7cf36f96f8ae692
bastiaanvonmeijenfeldt <42934890+bastiaanvonmeijenfeldt@users.noreply.github.com>
2018-10-24 15:04:35 +0200

    Update about.py

--- CAT_pack/about.py
+++ CAT_pack/about.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-__author__ = 'F.A. Bastiaan von Meijenfeldt'
-__version__ = '4.0'
-__date__ = '20th of September, 2018'
+__author__ = 'F. A. Bastiaan von Meijenfeldt'
+__version__ = '4.0.1'
+__date__ = '24th of October, 2018'
