82d2aaee3307ecddd224620130555cf8e87cef37
Bastiaan von Meijenfeldt <42934890+bastiaanvonmeijenfeldt@users.noreply.github.com>
2025-10-14 18:36:36 +0200

    Update about.py

--- CAT_pack/about.py
+++ CAT_pack/about.py
@@ -4,4 +4,4 @@ __authors__ = [
         "F. A. Bastiaan von Meijenfeldt", "Nikos Pappas", "Ernestina Hauptfeld"
         ]
 __version__ = "6.1beta"
-__date__ = "8 October, 2025"
+__date__ = "14 October, 2025"
